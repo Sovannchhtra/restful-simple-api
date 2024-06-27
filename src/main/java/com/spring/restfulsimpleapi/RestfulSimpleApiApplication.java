3f36854ac309d608095cfb860220bfab9e267a77
macOS <macos@MacBook-Pro-3.local>
2024-06-27 10:23:02 +0700

    done update data

--- src/main/java/com/spring/restfulsimpleapi/RestfulSimpleApiApplication.java
+++ src/main/java/com/spring/restfulsimpleapi/RestfulSimpleApiApplication.java
@@ -1,9 +1,24 @@
 package com.spring.restfulsimpleapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Data Analytic Restful API",
+                description = "This is a free api for public usage!",
+                contact = @Contact(
+                        name = "Nai Sovannchhaktra",
+                        email = "sovannchaktra@gmail.com",
+                        url = "www.facebook.com/tra"
+                )
+        )
+
+)
 public class RestfulSimpleApiApplication {
 
     public static void main(String[] args) {
